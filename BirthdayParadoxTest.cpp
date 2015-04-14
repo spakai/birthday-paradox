bc4ab85d547707d762c09e97ef768527f30c4711
Prakash <spakai@gmail.com>
2015-04-14 14:00:17 +0800

    Testing the listener

--- BirthdayParadoxTest.cpp
+++ BirthdayParadoxTest.cpp
@@ -2,6 +2,9 @@
 
 #include "BirthdayParadox.h"
 #include <iostream>
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
 
 using namespace testing;
 
@@ -37,7 +40,7 @@ TEST(BirthdayParadoxTest, AreTheNumbersReallyRandom) {
     ASSERT_THAT(list1 == list2, Eq(0));
 }
 
-TEST(BirthdayParadoxTest, Simulate) {
+TEST(BirthdayParadoxTest, SimulateWithSingleThreadedPool) {
 
     class SingleThreadedPool : public ThreadPool {
         public:
@@ -45,8 +48,10 @@ TEST(BirthdayParadoxTest, Simulate) {
                 work.execute();
             }
     };
+
     std::shared_ptr<ThreadPool> pool;
-    pool= std::make_shared<SingleThreadedPool>(); 
+    pool = std::make_shared<SingleThreadedPool>(); 
+    pool->start(0);
     BirthdayParadox birthday(10,{10,23,30,40,50});
     birthday.useThreadPool(pool);
     BirthdayParadoxListener listener;
@@ -54,3 +59,42 @@ TEST(BirthdayParadoxTest, Simulate) {
         
     ASSERT_THAT(listener.getSize(),Eq(5));
 }
+
+TEST(BirthdayParadoxTest, Listener) {
+
+    class CountingListener : public BaseListener {
+        public:
+            void update(int id, int duplicates) override {
+                std::unique_lock<std::mutex> lock(m);
+                count++;
+                std::cout << count << std::endl;
+                wasExecuted.notify_all(); 
+            }
+
+            void waitForNotificationOrFailOnTimeout(unsigned expectedCount, int milliseconds=10000) {
+                std::unique_lock<std::mutex> lock(m);
+                ASSERT_THAT(wasExecuted.wait_for(lock, std::chrono::milliseconds(milliseconds), [&] { return count == expectedCount; }), Eq(true));      
+            } 
+
+            std::condition_variable wasExecuted;
+            unsigned int count{0};
+            std::mutex m;
+    };
+
+    CountingListener countingListener;
+    std::shared_ptr<ThreadPool> pool;
+    std::shared_ptr<std::thread> t;
+    pool = std::make_shared<ThreadPool>(); 
+    BirthdayParadox birthday(100000,{10,23,30,40,50});
+    birthday.useThreadPool(pool);
+    pool->start(4);
+ 
+    t = std::make_shared<std::thread> (
+        [&] { birthday.simulate(countingListener);} 
+    );
+
+    t->join();
+    countingListener.waitForNotificationOrFailOnTimeout(5);
+ 
+}
+
