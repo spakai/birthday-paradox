082cfa0c6310881a1893bca1e9fcc56e795e8e0f
Prakash <spakai@gmail.com>
2015-08-28 20:33:40 +0800

    Cleaned up Listener class

--- Listener.h
+++ Listener.h
@@ -10,7 +10,7 @@ class BaseListener {
                                                                                                               
 class BirthdayParadoxListener : public BaseListener {                                                         
     public:                                                                                                   
-        virtual void update(int popSize, int duplicates) {                                                    
+        void update(int popSize, int duplicates) override{                                                    
             std::lock_guard<std::mutex> guard(m);                                                             
             gnuplotter.add(popSize, duplicates);                                                              
         }                                                                                                     
